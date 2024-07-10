be5c1a2a3b70e073b0506a9d88023b68edb07f0d
SalluuuBhai <md.salmanfaris03@gmail.com>
2024-07-10 23:54:56 +0530

    2nd commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,14 @@ const Home = () => {
           <div className="home_content flex items-center">
             <div
               className="avatar min-w-[300px] min-h-[300px] relative rounded-full"
+              style={{ marginTop: '60px' }}
               data-type="wave"
             >
               {" "}
               {/* data-type values are: "wave", "circle", "square"*/}
               <div
                 className="image absolute inset-0 bg-no-repeat bg-center bg-cover"
+                // style={{ marginTop: '30px' }}
                 data-img-url="assets/img/slider/salman.jpg"
               />
             </div>
@@ -25,7 +27,7 @@ const Home = () => {
               <p className="job font-montserrat font-medium max-w-[450px] mb-[25px]">
               As a Full-stack JS Developer, I harness JavaScript, React, Next.js, Angular, and Node.js to build engaging, user-centric web applications. I am knowledgeable about backend and frontend development requirements, including database management. I handle any part of the process with ease. I am a collaborative team player with a willingness to learn and grow with the organization. Let's make technology work for us by creating solutions that are both innovative and accessible.
               </p>
-              <div className="social w-full float-left">
+              <div className="social w-full float-left" style={{ marginBottom: '150px' }}>
                 <ul className="m-0 list-none">
                   {socialIcon.map((item) => (
                     <li className="mr-[2px] inline-block" key={item.id}>
